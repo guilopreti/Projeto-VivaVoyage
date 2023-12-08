968e972819ad7b3568e559e427c45168fe8a316e
Luiz Fernando <luiz.fpimentel0@gmail.com>
2023-12-08 17:03:27 -0300

    feat: criando função para adicionar nome do usuário logado no header

---
+++ js/setUsername.js
@@ -0,0 +1,11 @@
+const username = window.localStorage.getItem('usuario')
+if (username) {
+  const usernameSpan = document.querySelector('.loginLabel')
+  usernameSpan.innerHTML = `Olá ${username} <a onclick="logout()">(Sair)</a>`
+  usernameSpan.parentElement.href = '#'
+}
+
+function logout() {
+  window.localStorage.removeItem('usuario')
+  location.href = '/modelo.html'
+}
